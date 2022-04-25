d029875b71fae3df1c54fc3bda06b7f35aee6f80
Lewis Taylor <lewis@dangerfarms.com>
2022-04-25 19:29:16 +0100

    Put black squares to front

--- src/components/react/CubeRenderer.js
+++ src/components/react/CubeRenderer.js
@@ -471,7 +471,7 @@ function Boxes(props) {
 
     const _thickness = (previewCube ? thickness - 0.0001 : thickness) || 0.00001; // requested by client.
 
-    const _explosion = previewCube ? explosion - 0.01 : explosion;
+    const _explosion = previewCube ? explosion + 0.08 : explosion;
 
     const absoluteThickness = halfCubeSide * _thickness;
 
